d3aac7d585305a1b1ad3b043efac65b71b4dd27f
Kanne Samrudhi <134263151+Kanne-Samrudhi@users.noreply.github.com>
2024-12-20 20:44:25 +0530

    Add files via upload

---
+++ Pattern7.java
@@ -0,0 +1,20 @@
+package comm;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		// Taking user input for the number of rows
+		System.out.print("Enter a number: ");
+		int n = scanner.nextInt();
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+" ");
+			}
+			System.out.println();
+		}
+		
+}
+}
